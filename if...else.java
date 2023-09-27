e7e12ff393f5aae9482a6968649c08887ce6b756
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-09-27 13:16:16 -0400

    Create if...else.java

---
+++ if...else.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int i=4;
+    if(i%2==0) //if block
+    {
+      //ture statement
+      System.out.println("i is even number ");
+    }
+    else //else block
+    { //false statement
+      System.out.println("i is odd number "); 
+    }
+  }
+}

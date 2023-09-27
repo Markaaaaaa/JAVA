70410de177ccbf60ddc64687191b1eb8c1d51548
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-09-27 13:37:27 -0400

    Create nested if.java

---
+++ if ststatements/nested if.java
@@ -0,0 +1,19 @@
+
+class Main {
+  public static void main(String[] args) {
+    
+    int s=24;
+    if(s>30)
+    {
+      System.out.println("Number is greater than 30");  
+    }
+      if(s>20)
+      {
+         System.out.println("Number is greater than 20");  
+      }
+        if(s>10)
+        {
+          System.out.println("Number is greater than 10");  
+        }
+  }
+}

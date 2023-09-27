b55100322abce218d82f6efb654bf436874b7f89
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-09-27 13:52:57 -0400

    Create else if.java

---
+++ if ststatements/else if.java
@@ -0,0 +1,19 @@
+
+class Main {
+  public static void main(String[] args) {
+    
+    int s=24;
+    if(s>30)
+    {
+      System.out.println("Number is greater than 30");  
+    }
+    else if(s>20)
+    {
+         System.out.println("Number is betwwen 30 and 20");  
+    }
+    else if(s>10)
+    {
+        System.out.println("Number is betwwen 20 and 10");  
+    }
+  }
+}

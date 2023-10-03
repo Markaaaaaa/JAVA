c9ef64596ccfaacb5ba648a1a112a853b3c98f9c
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-10-03 13:57:23 -0400

    Create find even odd number.java

---
+++ if ststatements/find even odd number.java
@@ -0,0 +1,15 @@
+
+class Main {
+  public static void main(String[] args) {
+    
+    int nub=33;
+    if(nub%2==0)
+    {
+      System.out.println(nub+ " is a even number");
+    }
+    else 
+    {
+      System.out.println(nub+ " is a odd number");  
+    }
+  }
+}

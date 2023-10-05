f065148ba929be33299264b614d326a547e6b3c6
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-10-05 13:00:45 -0400

    Create else if for Grade.java

---
+++ if ststatements/else if for Grade.java
@@ -0,0 +1,27 @@
+//program to display the grade as student
+class Main {
+  public static void main(String[] args) {
+    
+    int nub=99;
+    if(nub>90)
+    {
+      System.out.println(nub+ " Grade A");
+    }
+    else if (nub>80)
+    {
+      System.out.println(nub+ " Grade B");  
+    }
+    else if (nub>70)
+    {
+      System.out.println(nub+ " Grade C");  
+    }
+    else if (nub>60)
+    {
+      System.out.println(nub+ " Grade D");  
+    }
+    else
+    {
+      System.out.println("you loser");
+    }
+  }
+}

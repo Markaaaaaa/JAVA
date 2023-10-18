b0db3411e17bab1545602dfeec813a44c39dfd33
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-10-18 13:41:45 -0400

    Create basic.java

---
+++ Loop/basic.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    //while loop
+    int i=0;
+    while(i<5){
+      System.out.println(i);
+      i++;
+    }
+    //for loop
+    for(int j=0; j<5; j++)
+    {
+      System.out.println(j);
+     }
+    
+  }
+}

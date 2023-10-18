7aef1b0d3f8b1ee387fd588ba095bfb9c8006670
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-10-17 21:26:32 -0400

    Create basic int and char.java

---
+++ Switch case/basic int and char.java
@@ -0,0 +1,29 @@
+
+class Main {
+  public static void main(String[] args) { 
+    /*int x=60;
+    switch(x)
+    {
+      case 40:
+        System.out.println("x is 40");
+        break;
+      case 60:
+        System.out.println("x is 60");
+        break;
+      default:
+        System.out.println("x is not 40, 60, or 80");
+    }*/
+    char x='s';
+    switch(x)
+    {
+      case 's':
+        System.out.println("x is s");
+        break;
+      case 'd':
+        System.out.println("x is d");
+        break;
+      default:
+        System.out.println("x is not s, d");
+    }
+  }
+}

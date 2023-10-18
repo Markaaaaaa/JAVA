fb5e56c4b41d748635a09dd18b09ff3b281f725e
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-10-18 13:43:16 -0400

    Create basic变体.java

---
+++ Loop/basic变体.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    //while loop
+    int i=15;
+    while(i>9){
+      System.out.println(i);
+      i-=1;
+    }
+    //for loop
+    for(int j=0; j<5; j++)
+    {
+      System.out.println(j);
+     }
+    
+  }
+}

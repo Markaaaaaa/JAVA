81c533595beca678d35be876aa7344d308bff804
Markaaaaaa <115102897+Markaaaaaa@users.noreply.github.com>
2023-10-26 13:26:44 -0400

    Create 用循环计算.java

---
+++ Loop/用循环计算.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    for(int num1=1;num1<=4;num1++){
+      System.out.println("\\  /");
+      System.out.println("/  \\");
+    }
+    int highTemp=5;
+    for(int i=-3;i<=highTemp/2;i++){
+      System.out.print((i*1.8+32)+"\t");
+    }
+  }
+}
+/*
+"\t" is a tab 
+"\n" is a new line
+"\r" is a carriage return
+"\b" is a backspace
+"\f" is a form feed
+"\'" is a single quote
+"\"" is a double quote
+"\\" is a backslash
+*/
